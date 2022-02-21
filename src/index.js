116ef19e15618b01f9eb090ef363f3207d02cb59
Amine Machfer <74466395+aminemach@users.noreply.github.com>
2022-02-21 11:10:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 import loadComponents from './components';
 import loadBlocks from './blocks';
 
-export default grapesjs.plugins.add('grapesjs-plugin-carousel', (editor, opts = {}) => {
 
-    const options = {...{
-                blocks: ['carousel', 'mpg'],
+export default (editor, opts = {}) => {
+  const options = { ...{
+    i18n: {},
+    
+    
+  },  ...opts };
 
-                prefixName: 'bst-carousel',
 
-                gridsCategory: 'Extra',
+  loadComponents(editor, options);
+  loadBlocks(editor, options);
+ 
 
-                autoplay: true,
-
-                interval: 5000,
-
-                slides: 3,
-
-                mpgCss: 'https://ettrics.github.io/material-photo-gallery/dist/css/material-photo-gallery.css',
-
-                mpgJs: 'https://ettrics.github.io/material-photo-gallery/dist/js/material-photo-gallery.min.js'
-        }, ...opts};
-
-    // Add components
-    loadComponents(editor, options);
-
-    // Add blocks
-    loadBlocks(editor, options);
-});
+  
+  
+};
