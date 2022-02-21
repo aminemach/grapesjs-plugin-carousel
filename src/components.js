bfba2e05de1148f5165dc77d8178e7c72e6a931c
Amine Machfer <74466395+aminemach@users.noreply.github.com>
2022-02-21 13:46:03 +0100

    Update components.js

--- src/components.js
+++ src/components.js
@@ -18,14 +18,14 @@ function showDivs(n) {
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";
 }
         }
           }
       },
-    },
+
     isComponent: (el) => {
       if (el.className && el.className.includes("carouselContainer")) {
         return {
@@ -35,5 +35,5 @@ function showDivs(n) {
     },
   });
 
-  
+
 };
