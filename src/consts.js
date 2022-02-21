e39951d7e1d5be4e1f2134d0f7dc1954c180dd82
Amine Machfer <74466395+aminemach@users.noreply.github.com>
2022-02-21 13:25:31 +0100

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 export const 
 
 
- SwiperStyle = function()  {
+ carouselstyle = function()  {
 
     return ` <style>
 
@@ -350,57 +350,67 @@ export const
     
     return `
       <style>
-      .columns {
-        float: left;
-        width: 33.3%;
-        padding: 8px;
-      }
-      
-      .price {
-        list-style-type: none;
-        border: 1px solid #eee;
-        margin: 0;
-        padding: 0;
-        -webkit-transition: 0.3s;
-        transition: 0.3s;
-      }
-      
-      .price:hover {
-        box-shadow: 0 8px 12px 0 rgba(0,0,0,0.2)
-      }
-      
-      .price .header {
-        background-color: #111;
-        color: white;
-        font-size: 25px;
-      }
-      
-      .price li {
-        border-bottom: 1px solid #eee;
-        padding: 20px;
-        text-align: center;
-      }
-      
-      .price .grey {
-        background-color: #eee;
-        font-size: 20px;
-      }
-      
-      .button {
-        background-color: #04AA6D;
-        border: none;
-        color: white;
-        padding: 10px 25px;
-        text-align: center;
-        text-decoration: none;
-        font-size: 18px;
-      }
-      
-      @media only screen and (max-width: 600px) {
-        .columns {
-          width: 100%;
-        }
-      }
+     
+             .carouselContainer {
+              width: 60%;
+              height: 90%;
+              margin: 0 auto;
+              position: relative;
+              background-color: #fff;
+              border-radius: 1.5rem;
+              overflow: hidden;
+            }
+
+            .carouselImgs {
+              width: 100%;
+              height: 100%;
+              animation: fade 1.5s;
+              display: none;
+              padding: 20px;
+              text-align: center;
+              align-items: center;
+              justify-items: center;
+              justify-content: center;
+            }
+
+            .prev, .next {
+              position: absolute;
+              top: 49%;
+              cursor: pointer
+            }
+
+            .prev {
+              left: 10px;
+            }
+
+            .next {
+              right: 10px;
+            }
+
+            @keyframes fade {
+              from {
+                opacity: 0.4;
+              }
+              to {
+                opacity: 1;
+              }
+            }
+
+            .slide1 {
+              background-color: #d8e2dc
+            }
+
+            .slide2 {
+              background-color: #577399
+            }
+
+            .slide3 {
+              background-color: #bdd5ea;
+            }
+
+            .slide4 {
+              background-color: #f7f7ff;
+            }
       </style>
     `
 
